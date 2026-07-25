2583133bc59e7a2b2a6113fc2d7b35a8cf4956a0
Anne Leina <annelenku@gmail.com>
2026-07-25 23:18:41 +0300

    Remove OrderConfirmation temporarily for deployment

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,6 @@ import Home from './pages/Home';
 import Products from './pages/Products';
 import Cart from './pages/Cart';
 import Checkout from './pages/Checkout';
-import OrderConfirmation from './pages/OrderConfirmation';
 
 export default function App() {
   return (
@@ -16,7 +15,6 @@ export default function App() {
         <Route path="/products" element={<Products />} />
         <Route path="/cart" element={<Cart />} />
         <Route path="/checkout" element={<Checkout />} />
-        <Route path="/order-confirmation/:orderNumber" element={<OrderConfirmation />} />
       </Routes>
       <Footer />
     </BrowserRouter>
